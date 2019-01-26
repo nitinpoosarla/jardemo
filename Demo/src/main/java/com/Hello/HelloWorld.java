fc72cb87d9c4439fc207b7dbb9f067e72fba6e87
nitin <nithin.poosarla@gmail.com>
2019-01-27 01:41:07 +0530

    new files added

--- Demo/src/main/java/com/Hello/HelloWorld.java
+++ Demo/src/main/java/com/Hello/HelloWorld.java
@@ -1,11 +1,36 @@
 package com.Hello;
 
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+
+
+
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		System.out.println("nitin world");
+		HelloWorld hw = new HelloWorld();//src/
+		System.out.println(hw.getFileWithUtil("parcel/parcel1.txt"));
+		System.out.println("nitin world"+hw.getFileWithUtil(args[0]));
 
 	}
+
+	private String  getFileWithUtil(String fileName) {
+		String result = "";
+		
+		ClassLoader classLoader = getClass().getClassLoader();
+		try {
+		    result = IOUtils.toString(classLoader.getResourceAsStream(fileName));
+		//	String result1  = classLoader.getResource("parcel1.txt").getPath();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+			
+		return result;
+	  }
+	
 
 }
